825106d0439bf847cc18501a24483e7c5e6d7bf2
Reza-Riswandha <rezariswan29@gmail.com>
2024-03-31 23:11:48 +0700

    add variabel if/else in kontak controller

--- lib/controller/kontak_controller.dart
+++ lib/controller/kontak_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:activity2/model/kontak.dart';
@@ -13,7 +14,31 @@ class KontakController {
       'alamat': person.alamat,
       'telepon': person.telepon,
     };
-    
-    
+
+    try {
+      var response = await kontakService.addPerson(data, file);
+
+      if (response.statusCode == 201) {
+        return {
+          'success': true,
+          'message': 'Data berhasil disimpan',
+        };
+      } else {
+        if (response.headers['content-type']!.contains('application/json')) {
+          var decodedJson = jsonDecode(response.body);
+          return {
+            'success': false,
+            'message': decodedJson['message'] ?? 'Terjadi kesalahan',
+          };
+        }
+
+        
+      }
+    } catch (e) {
+      return {
+        'success': false,
+        'message': 'Terjadi kesalahan: $e',
+      };
+    }
   }
 }
