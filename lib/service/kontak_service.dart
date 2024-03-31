b0f33b0e88d18bd4edf4b008572290eb85db1d37
Reza-Riswandha <rezariswan29@gmail.com>
2024-03-31 22:30:21 +0700

    add Uri get Uri in KontakService

--- lib/service/kontak_service.dart
+++ lib/service/kontak_service.dart
@@ -2,4 +2,8 @@ class KontakService {
   final String baseUrl = 'https://paml.tatiumy.com/api/';
   final String endpoint = 'people';
 
+  Uri getUri(String path) {
+    return Uri.parse("$baseUrl$path");
+  }
+
 }
