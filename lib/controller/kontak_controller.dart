136ecc48d8fc64d4bd9b439d1d51387c41a71bb4
Reza-Riswandha <rezariswan29@gmail.com>
2024-03-31 22:51:22 +0700

    add class Kontak Service

--- lib/controller/kontak_controller.dart
+++ lib/controller/kontak_controller.dart
@@ -1,50 +1,5 @@
 
 
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:activity2/model/kontak.dart';
-
 class KontakController {
-  final kontakService = KontakController();
-
-  Future<Map<String, dynamic>> addPerson(Kontak person, File? file) async {
-    Map<String, String> data = {
-      'nama': person.nama,
-      'email': person.email,
-      'alamat': person.alamat,
-      'telepon': person.telepon,
-    };
-
-    try {
-      var response = await kontakService.addPerson(data, file);
-
-      if (response.statusCode == 201) {
-        return {
-          'success': true,
-          'message': 'Data berhasil disimpan',
-        };
-      } else {
-        if (response.headers['content-type']!.contains('application/json')) {
-          var decodedJson = jsonDecode(response.body);
-          return {
-            'success': false,
-            'message': decodedJson['message'] ?? 'Terjadi kesalahan',
-          };
-        }
-
-        var decodedJson = jsonDecode(response.body);
-        return {
-          'success': false,
-          'message':
-              decodedJson['message'] ?? 'Terjadi kesalahan saat menyimpan data',
-        };
-      }
-    } catch (e) {
-      return {
-        'success': false,
-        'message': 'Terjadi kesalahan: $e',
-      };
-    }
-  }
+ 
 }
