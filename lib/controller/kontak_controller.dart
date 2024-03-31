c097d4548379213b83cd975d4dde8540406e0e37
Reza-Riswandha <rezariswan29@gmail.com>
2024-03-31 23:01:01 +0700

    Add future addperson in Kontak controller

--- lib/controller/kontak_controller.dart
+++ lib/controller/kontak_controller.dart
@@ -1,5 +1,19 @@
+import 'dart:io';
+
+import 'package:activity2/model/kontak.dart';
 import 'package:activity2/service/kontak_service.dart';
 
 class KontakController {
   final kontakService = KontakService();
+
+  Future<Map<String, dynamic>> addPerson(Kontak person, File? file) async {
+    Map<String, String> data = {
+      'nama': person.nama,
+      'email': person.email,
+      'alamat': person.alamat,
+      'telepon': person.telepon,
+    };
+    
+    
+  }
 }
