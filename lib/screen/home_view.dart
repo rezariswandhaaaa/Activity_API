764ea1d46d155e5adc27ff1548b284b1ba4be269
Reza-Riswandha <rezariswan29@gmail.com>
2024-04-01 14:24:34 +0700

    Add body in Home View

--- lib/screen/home_view.dart
+++ lib/screen/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:activity2/controller/kontak_controller.dart';
+import 'package:activity2/model/kontak.dart';
+import 'package:activity2/screen/register_view.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -23,6 +26,31 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         title: const Text("Daftar Orang"),
       ),
+      body: FutureBuilder<List<Kontak>>(
+        future: _controller.getPeople(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: const CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                Kontak kontak = snapshot.data![index];
+                return ListTile(
+                  title: Text(kontak.nama),
+                  subtitle: Text(kontak.email),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(kontak.foto),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+      
     );
   }
 }
