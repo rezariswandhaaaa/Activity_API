ea01561056b1e68cedcb272d04e1e86d9cd576be
Reza-Riswandha <rezariswan29@gmail.com>
2024-04-01 12:49:43 +0700

    Add future fetch people in kontak service

--- lib/service/kontak_service.dart
+++ lib/service/kontak_service.dart
@@ -1,7 +1,8 @@
-
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+
 class KontakService {
   final String baseUrl = 'https://paml.tatiumy.com/api/';
   final String endpoint = 'people';
@@ -11,20 +12,32 @@ class KontakService {
   }
 
   Future<http.Response> addPerson(Map<String, String> data, File? file) async {
-    var request = http.MultipartRequest(
-      'POST', 
-      getUri(endpoint)
-      )
-        ..fields.addAll(data)
-        ..headers['Content-Type'] = 'application/json';
+    var request = http.MultipartRequest('POST', getUri(endpoint))
+      ..fields.addAll(data)
+      ..headers['Content-Type'] = 'application/json';
 
-    if (file != null){
+    if (file != null) {
       request.files.add(await http.MultipartFile.fromPath('gambar', file.path));
-    }
-    else{
+    } else {
       request.files.add(http.MultipartFile.fromString('gambar', ''));
     }
 
     return await http.Response.fromStream(await request.send());
+  }
+
+  Future<List<dynamic>> fetchPeople() async {
+    var response = await http.get(
+        getUri(
+          endpoint,
+        ),
+        headers: {
+          "Accept": "application/json",
+        });
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodeResponse = json.decode(response.body);
+      return decodeResponse['people'];
+    } else {
+      throw Exception('Failed to load people: ${response.reasonPhrase}');
+    }
   }
 }
